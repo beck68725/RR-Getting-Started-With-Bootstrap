22ed5d6cf78023cf2da87355b1bc5aa31a84f659
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-07-14 20:03:45 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,114 @@
 import './App.css';
-
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
 function App() {
+  
   return (
     <div className="App">
-      <h1>Bootstrap Website</h1>
-     
+      <Button variant="outline-info">Bootstrap Website</Button>{''}
+      <Card>
+      <Card.Header>
+        <Nav variant="tabs" defaultActiveKey="#first">
+          <Nav.Item>
+            <Nav.Link href="#first">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+      <Form>
+      <Form.Group>
+    <Form.Label>Form Dropdown with favorite movies</Form.Label>
+    <Form.Control as="select">
+      <option>Inception</option>
+      <option>Forrest Gump</option>
+      <option>Shawshank Redemption</option>
+      <option>Spiderman: Into the Spiderverse</option>
+      <option>Avengers: Endgame</option>
+    </Form.Control>
+</Form.Group>
+
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Form.Group controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+</Form>
+<Nav activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Home</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="/about">About Us</Nav.Link>
+    </Nav.Item>
+</Nav>
     </div>
   );
 }
 
 export default App;
+
+
+ 
